c96ffd11fbe42bc6e7e8c975ddafefcb4ff59298
Jaytee Padilla <padilla.jaytee@gmail.com>
2019-05-15 15:21:36 -0700

    Menu Component complete

--- Article/Article.js
+++ Article/Article.js
@@ -15,7 +15,6 @@ class Article {
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
-    console.log(this.domElement);
   }
 }
 
